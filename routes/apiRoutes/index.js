fd3c5ed412556dd01dee3f8022b3b021093b414b
Steven Nguyen <qviet715@gmail.com>
2023-03-19 20:49:22 -0400

    api routes

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,32 @@
+
+const router = require("express").Router();
+const db = require("../../db");
+
+// requesting data from readNotes
+router.get("/notes", (req, res) => {
+  db.readNotes().then((notes) => {
+    return res.json(notes);
+  });
+});
+
+// submitting data by writing (req.body) onto the front end
+router.post("/notes", (req, res) => {
+  db.writeNote(req.body).then((notes) => {
+    return res.json(notes);
+  });
+});
+
+//delete section
+router.delete("/notes/:id", (req, res) => {
+  db.removeNote(req.params.id)
+    .then( () => {
+      res.json({
+        ok: true,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
